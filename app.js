00e9be3c6c8f7e1a1fdcde8331be8031de1b85a6
Michal Novemsky <michal.novemsky@gmail.com>
2015-11-10 15:02:51 -0500

    Added chalk && application-level middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+var chalk = require('chalk');
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next){
+    console.log(chalk.bgBlue(req.method) + " " + chalk.magenta(req.url) + " " + chalk.green(res.statusCode));
+    next();
+
+});
+
 app.get('/', function(req, res, next){
-  res.send("Hello World!");
+ // res.send("Hello World!");
+ res.sendStatus(200);
+  //res.send(req);
 });
 
 app.listen(3000, function(req, res, next){
