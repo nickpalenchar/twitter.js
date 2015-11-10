a8807c76d6fe72177e824b770973798e44654b7e
Nick <npalenchar@gmail.com>
2015-11-10 15:33:28 -0500

    added swig functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var chalk = require('chalk');
 var express = require('express');
 var app = express();
+var swig = require('swig');
 
 app.use(function(req, res, next){
     console.log(chalk.bgBlue(req.method) + " " + chalk.magenta(req.url) + " " + chalk.green(res.statusCode));
@@ -9,8 +10,20 @@ app.use(function(req, res, next){
 });
 
 app.get('/', function(req, res, next){
- // res.send("Hello World!");
- res.sendStatus(200);
+  // res.send("Hello World!");
+  var data = {
+    title: "An example",
+    people: [
+      {name: "Gandalf"},
+      {name: "Frodo"},
+      {name: "Hermione"}
+    ]
+  };
+  swig.renderFile(__dirname + '/views/index.html', data, function(err, output){
+    if(err) throw err;
+    res.send(output);
+  });
+  //res.sendStatus(200);
   //res.send(req);
 });
 
